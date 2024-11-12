c4792ae34074d349431687daf87b9ca5c02ddb6a
Nutto.jsx <91049342+ntdotjsx@users.noreply.github.com>
2024-11-12 16:14:46 +0700

    ...

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,16 @@ author 'ntdotjsx'
 
 version '1.0'
 
+ui_page 'dist/index.html'
+
+files {
+    'dist/index.html',
+    'dist/assets/index.css',
+    'dist/assets/app.js'
+}
+
+client_script 'core/main.lua'
+
 shared_scripts {
 	'@es_extended/imports.lua',
 	'@es_extended/locale.lua',

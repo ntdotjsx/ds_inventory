bad7eea365e3dd5994ed2fea79886f3ff3893abc
Nutto.jsx <91049342+ntdotjsx@users.noreply.github.com>
2024-11-12 20:47:34 +0700

    xxx

--- interface/vite.config.js
+++ interface/vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // กำหนดการตั้งค่าเพิ่มเติมได้ที่นี่
-  base: './', // กำหนดให้ base เป็น relative path เพื่อให้ทำงานร่วมกับ Lua ใน FiveM ได้
+  optimizeDeps: {
+    include: ['jquery', 'jquery-ui-dist']
+  },
+  base: './',
   build: {
-    outDir: '../dist', // กำหนดโฟลเดอร์ output เมื่อ build
-    emptyOutDir: true, // ลบไฟล์ในโฟลเดอร์ outDir ก่อน build ใหม่
+    outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/app.js', // กำหนดชื่อไฟล์ JS หลัก
-        chunkFileNames: 'chunks/[name]-[hash].js', // กำหนดชื่อไฟล์ JS chunk
-        assetFileNames: 'assets/[name][extname]' // กำหนดชื่อไฟล์ asset (เช่น CSS, รูปภาพ)
+        entryFileNames: 'assets/app.js',
+        chunkFileNames: 'chunks/[name]-[hash].js',
+        assetFileNames: 'assets/[name][extname]'
       }
     }
-  },
-  // อื่น ๆ เช่น plugins, server settings สามารถเพิ่มได้ที่นี่ตามต้องการ
+  }
 });
